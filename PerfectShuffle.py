87ae97fe765665133fa1825c614edd68b2612216
dom demeo <dd3780@mcla.edu>
2017-03-27 03:12:12 +0000

    upload

--- PerfectShuffle.py
+++ PerfectShuffle.py
@@ -1,17 +1,17 @@
 import Epic 
 
-Value = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+Value = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']              #Defines values of deck an suit of cards 
 Suit = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
 
 def CreateDeck(Value, Suit):
-    NormalDeck = ["%s of %s" % (v, s) for v in Value for s in Suit]
+    NormalDeck = ["%s of %s" % (v, s) for v in Value for s in Suit]             #combines the suit and the vaules to create 52 cards with different values
     return NormalDeck
 
 def Shuffle(NormalDeck):
     SplitDeck1 = NormalDeck[:26]
     SplitDeck2 = NormalDeck[26:]
     
-    ShuffledDeck = []
+    ShuffledDeck = []                               #interlaces half the deck with the otehr half and only does 52 cards
 
     for i in range(26):
         ShuffledDeck.append(SplitDeck1[i])
@@ -19,10 +19,10 @@ def Shuffle(NormalDeck):
     return ShuffledDeck
     
 def Deal(ShuffledDeck):
-    Hand = ShuffledDeck[:5]
+    Hand = ShuffledDeck[:5]             #deals top 5 card in the new shuffled deck
     return Hand
     
-def main():
+def main():                                         #creates the deck then shuffles it then prints out the top 5 cards to the user after they enter the number of shuffles they desire 
     NormalDeck = CreateDeck(Value, Suit)
     DeckShuffles = 0;
     NumberOfShuffles = Epic.userInt("Please Enter Number of Times You Would Like The Deck Shuffled: ")
