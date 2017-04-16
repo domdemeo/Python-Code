2554ce028ec323653747a352a80e0446d52f28bf
dom demeo <dd3780@mcla.edu>
2017-04-16 22:04:37 +0000

    upload

--- Exam5/Exam5.py
+++ Exam5/Exam5.py
@@ -14,25 +14,26 @@ def PetStoreSearch(Term, Search):
     Term = Term.title()
     Search= Search.title()
     PetStoreFile = ReadJson()
-    if Search == "Category":
+    if Search == "Category":                                
         for product in PetStoreFile:
             if product["Category"] == Term:
-                print "%s - $%s" % (product["Product"], product["Price"])
+                print "%s - $%s" % (product["Product"], product["Price"])       #Prints out either category or keyword you searched for along with the product under that category and price 
+    
     if Search == "Keyword":
         for product in PetStoreFile:
             if Term in product["Product"]:
                print "%s - $%s" % (product["Product"], product["Price"])
-    
+               
 def main():
     String = Epic.userString("Please Enter (c) or (k) to Search by category or keyword ?")
     Search = ""
     if String.lower() =='c':
-        Search = "category"
-        Term = Epic.userString("Enter a %s:" % Search)
-        PetStoreSearch(Term, Search)
-    if String.lower() == 'k':
-        Search = "keyword"
-        Term = Epic.userString("Enter a %s:" % Search)
+        Search = "Category (Food, Toys, Furniture)"
+        Term = Epic.userString("Enter a %s:" % Search)              #checks if c and then searches for what ever category your search for
         PetStoreSearch(Term, Search)
     
+    if String.lower() == 'k':
+        Search = "Keyword"
+        Term = Epic.userString("Enter a %s:" % Search)           #checks if Keyword and then searches for what ever Keyword your search for
+        PetStoreSearch(Term, Search)
 main()
