7d8b5b99e7177e0b3f2a8e3dd040aaca2baf7b4d
dom demeo <dd3780@mcla.edu>
2017-05-01 04:07:10 +0000

    upload

--- Exam6.py
+++ Exam6.py
@@ -3,15 +3,15 @@ import Epic
 Suspects = ["Miss Scarlet", "Col Mustard", "Mr Green"]
 Weapons = ["Candlestick","Wrench","Pipe"]
 
-def TotalPossibilities(Suspects, Weapons):
-    TotalPossibilities = []
+def TotalPossibilities(Suspects, Weapons):              
+    TotalPossibilities = []                             #Generates a list of al of the possible possibilities for both supects and murder weapons
     for s in Suspects:
         for w in Weapons:
             TotalPossibilities.append("%s with the %s" % (s, w))
     return TotalPossibilities
 
 def NarrowSuspects(Possibilities, Suspects, Weapons):
-    Clue = Epic.userString("Is the clue a suspect or a weapon? (Please Enter Either a S or W Only!):")
+    Clue = Epic.userString("Is the clue a suspect or a weapon? (Please Enter Either a S or W Only!):")   #section for users to enter either suspects and weapons
     if Clue.upper() == "S":
         SuspectorWeapon = Epic.userString("Enter the innocent person's name. (Please Enter name exactly as it Appears!) (%s):" % Suspects)
     if Clue.upper() == "W":
@@ -20,18 +20,17 @@ def NarrowSuspects(Possibilities, Suspects, Weapons):
         print "You have entered invalid input please enter exact input as instructed"
     InvalidInput = []
     for item in Possibilities:
-        if SuspectorWeapon.upper() in item.upper():
+        if SuspectorWeapon.upper() in item.upper():                 #clears invaild input and removes invalid items from the possiblites list
             InvalidInput.append(item)
     for i in InvalidInput:
         Possibilities.remove(i)
     return Possibilities
 
 def main():
-    Possibilities = TotalPossibilities(Suspects, Weapons)
-    
+    Possibilities = TotalPossibilities(Suspects, Weapons)           #creates total possiblities to be widdled down
     while len(Possibilities) > 1:
         print "There are currently %s possibilities left." % len(Possibilities)
-        NarrowPossibilities = NarrowSuspects(Possibilities, Suspects, Weapons)
+        NarrowPossibilities = NarrowSuspects(Possibilities, Suspects, Weapons)              #narrows supects and weapons and leaves total number of possiblites left and prints out results 
     print "There is only one possibility left."
     print "It was %s! who killed the Victim!" % Possibilities[0]
     
